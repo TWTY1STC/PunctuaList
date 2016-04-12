53f1e4ef49c4a8108071ef01a103b7f68659649b
twty1stc <liza.njuguna@gmail.com>
2016-04-12 18:19:04 +0000

    Add complete with delete ability

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,30 @@
 class ItemsController < ApplicationController
 
-  
   def create
     @item = current_user.items.new(item_params)
     
     if @item.save
-      flash[:notice] = "Item has been successfully added to your list"
+      flash.now[:notice] = "Item has been successfully added to your list"
     else
-      flash[:alert] = "There was an error saving your list item. Please try again"
+      flash.now[:alert] = "There was an error saving your list item. Please try again"
     end
      redirect_to root_path
   end
 
-private
-
-def item_params
-  params.require(:item).permit(:name)
-end
+  def destroy
+    @item = current_user.items.find(params[:id])
+    
+    if @item.destroy
+      flash.now[:notice] = "Way to go! Your completed item has been removed from your list."
+    else
+      flash.now[:alert] = "There was an error deleting your completed task. Please try again."
+    end
+    redirect_to root_path
+  end
+  
+  private
+  
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
